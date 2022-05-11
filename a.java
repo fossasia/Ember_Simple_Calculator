c4e5f78298e7a4edbab03a405a4b09f40c3ac680
Jamilul93 <jami1807093@stud.kuet.ac.bd>
2022-05-11 18:50:18 +0600

    java file created

---
+++ a.java
@@ -0,0 +1,22 @@
+class A {
+    String name ;
+    int number ;
+     public void set_name(String n){
+         name =n;
+     }
+     public void set_number(int s){
+         number=s;
+     }
+      public String  get_name(){
+         return name;
+     }
+     public int get_number(){
+         return number;
+     }
+  public A(String s1,int num) {
+       name=s1;
+       number=num;
+   }   
+  
+  
+  };
